a96dd45f3cc9c0f26d346681dea9ba2654d709a9
Ardacay <ardacay3@outlook.com>
2025-07-15 17:24:09 +0300

    Initial Commit

---
+++ UserManagmentProject/UserManagmentProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserManagmentProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
