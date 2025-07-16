4a92484d884dde89108533181e358e99cf78c887
Ardacay <ardacay3@outlook.com>
2025-07-16 16:00:27 +0300

    manager can be change roles
    the record is saved in the database as a member and the manager can change the roles.

--- UserManagmentProject/UserManagmentProject/Data/ApplicationDbContext.cs
+++ UserManagmentProject/UserManagmentProject/Data/ApplicationDbContext.cs
@@ -5,17 +5,28 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace UserManagmentProject.Data
 {
-    public class ApplicationDbContext:IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            :base(options)
+            : base(options)
         {
         }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
         }
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
 
     }
 }
